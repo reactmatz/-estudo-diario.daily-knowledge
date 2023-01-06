a9980ac185a40ca29f06170099f03a190683b5d2
reactmatz <matheusnasc.contato@gmail.com>
2023-01-05 22:56:49 -0300

    examples-js

--- prototype/script.js
+++ prototype/script.js
@@ -12,3 +12,25 @@ person1.sayHello(); // output: "Hello, my name is John"
 
 ///MAIS UM EXEMPLO
 
+function Circle(radius) {
+  this.radius = radius;
+}
+
+// Adicionar uma propriedade ao prototype
+Circle.prototype.diameter = function() {
+  return this.radius * 2;
+};
+
+// Adicionar um método ao prototype
+Circle.prototype.area = function() {
+  return Math.PI * this.radius * this.radius;
+};
+
+var circle1 = new Circle(5);
+console.log(circle1.diameter()); // output: 10
+console.log(circle1.area()); // output: 78.53981633974483
+
+// Adicionar uma propriedade a um objeto já existente
+circle1.color = "red";
+console.log(circle1.color); // output: "red"
+
