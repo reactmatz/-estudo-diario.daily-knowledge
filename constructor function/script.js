89450061b7014b3a1cb328f22a9b8d5e6dc4ee7e
reactmatz <matheusnasc.contato@gmail.com>
2023-01-15 22:20:02 -0300

    funcoes construtoras

---
+++ constructor function/script.js
@@ -0,0 +1,19 @@
+function Pessoa(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
+}
+
+let pessoa1 = new Pessoa("João", 30);
+let pessoa2 = new Pessoa("Maria", 25);
+
+function Pessoa(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
+  this.apresentar = function(){
+    console.log(`Olá, eu sou ${this.nome} e eu tenho ${this.idade} anos.`);
+  }
+}
+
+let pessoa1 = new Pessoa("João", 30);
+pessoa1.apresentar(); // Olá, eu sou João e eu tenho 30 anos.
+
