93c44067fdfda475ed9f1a61d591d04b019df204
reactmatz <matheusnasc.contato@gmail.com>
2023-01-17 21:50:09 -0300

    optional chaining

---
+++ optional chaining/script.js
@@ -0,0 +1,17 @@
+const user = {
+  name: "carlos",
+  age: 28,
+  adress: {
+    street: "Rua timbiras",
+    number: 285,
+    zipcode:"1112-2333"
+  }
+  
+}
+
+// if (user != undefined && user.adress != undefined){
+//   console.log(user.adress.street);
+// }
+
+// podemos escrever o mesmo codigo assim:
+
