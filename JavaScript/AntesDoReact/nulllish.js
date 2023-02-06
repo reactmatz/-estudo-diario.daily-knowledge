0b7e57f909f674da2b0ab71f9f697411336a092e
reactmatz <matheusnasc.contato@gmail.com>
2023-02-05 22:23:15 -0300

    Add: Tudo oque voce deve aprender antes de ir para o React

---
+++ JavaScript/AntesDoReact/nulllish.js
@@ -0,0 +1,16 @@
+//Nullish Coalescing Operator
+
+// quando voce quer lidar com variaveis que por sua vez existe um valor representado real nela, mas que o javascript interpreta como null, por ex o 0, ao inves de utilizar o operador || utilize o ?? pois ele vai ser mais restritivo e os valores nulos para ele vai ser algo como undefined ou null em si.
+
+// veja o exemplo abaixo
+
+var idade = 0;
+
+const compar = "Minha idade e : " +  (idade || 'nao informada');
+const teste = document.getElementById('minhaid').innerHTML = compar;
+
+// veja que 0 e um valor real, certo ? 0 continua sendo um numero, se voce trocar o ?? por || voce vai ver que o codigo vai retornar 'nao informado' quando na verdade queremos que retorne 0.
+
+// ou seja, nullish serve para quando vc precisar estabelecer um valor default para uma variavel que ainda nao foi for 'setada', ela ser estritamente precisa sobe oque e default ou nao, no caso de 0 iria retornar default com ||, oque nao queremos.
+
+
---
+++ JavaScript/AntesDoReact/objects.js
@@ -0,0 +1,22 @@
+// objetos em javascript
+
+const user = {
+  name:'matheus',
+  idade:23,
+  adress:{
+    street:'rua de tal',
+    number:253
+  }
+}
+// const validation = ('name' in user) ----- 
+// console.log(Object.keys(user));
+// console.log(Object.values(user))
+// console.log(Object.entries(user))  --- metodo que retorna o vetor [] com cada valor separados por virgula organizado como se fosse um objeto
+
+// ---- operador 'in' serve para validar se existe um objeto dentro outro
+// ---- Object.keys() e um metodo que retorna as chaves em vetor [] do objeto selecionado, o mesmo serve para Object.values() para retornar os valores
+
+
+// ------------------- //
+
+
